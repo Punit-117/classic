8e075ba3e46a2ac625df1aac8e8e839bf9f7e53a
Punit <punit.dixit.2020@ecajmer.ac.in>
2022-12-09 12:17:57 +0530

    UPdain add.file

--- add.java
+++ add.java
@@ -4,9 +4,12 @@ public class add{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
+        System.out.println("Enter the new number:")
          int b=sc.nextInt();
+        System.out.println("Enter the new number:")
 
-         System.out.println(a+b);
+
+         System.out.println("The sum is : "a+b);
 
 
     }
